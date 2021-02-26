ed281607ea5b9712573842705b35878388fbd2cb
rwietter <mauriciobw17@gmail.com>
2021-02-26 15:42:30 -0300

    add: contants messages

---
+++ src/utils/user.messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_GET_POSITION = `precisamos da sua localização paraentregar o pedido,
+			para continuar você deve permitir nas configurações do site.`;
+
+export const SUCCESS_GET_POSITION = `Pronto! já temos sua localização :)`;
+
+export const UNSUPPORTED_GEOLOCATION = `Geolocalização não é suportada neste dispositivo`;
