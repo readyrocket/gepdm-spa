f216fd441747d3f95539bff00f0c30bf60f4faca
rwietter <mauriciobw17@gmail.com>
2021-02-26 15:41:40 -0300

    fix: 404 route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,17 +4,20 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Loading } from '../components/Loading';
 import AppPrivateRoutes from './app.router';
 
-const Restaurants = lazy(() => import(`../pages/Restaurants`));
+const Homescreen = lazy(() => import(`../pages/Home`));
 
 const Landing = lazy(() => import(`../pages/Landing`));
 
+const Error = () => <h1>404: not found</h1>;
+
 const Routes = () => (
 	<BrowserRouter>
-		<Suspense fallback={<Loading/>}>
+		<Suspense fallback={<Loading />}>
 			<Switch>
 				<Route exact path="/" component={Landing} />
-				<AppPrivateRoutes to="/home" component={Restaurants} />
-				<AppPrivateRoutes to="/restaurants" component={Restaurants} />
+				<AppPrivateRoutes path="/home" component={Homescreen} />
+				<AppPrivateRoutes path="/restaurants" component={Homescreen} />
+				<Route path="*" component={Error} />
 			</Switch>
 		</Suspense>
 	</BrowserRouter>
